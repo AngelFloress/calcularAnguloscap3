a8b4760401e7592d17d29a059f2bfc198b037908
Angel Flores <89488077+AngelFloress@users.noreply.github.com>
2021-09-24 10:46:30 -0700

    Create Programa52.cpp

---
+++ Programa52.cpp
@@ -0,0 +1,16 @@
+# calcularAnguloscap3
+
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+short LA,LB;
+int area;
+void main(){
+cout<<"Ingrese el valor del lado A: ";
+cin>>LA;
+cout<<"Ingrese el valor de la altura  ";
+cin>>LB;
+area=(LB*LA);
+cout<<"El area es: "<<area;
+getch();
+           }
